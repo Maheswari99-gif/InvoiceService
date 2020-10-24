c70defe238f19091c9ab22ebcc92f80b0e4ff4c7
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-24 23:06:45 +0530

    MaheswariKoyyani : create Invoice summary

--- src/main/java/com/capgemini/invoiceservice/InvoiceService.java
+++ src/main/java/com/capgemini/invoiceservice/InvoiceService.java
@@ -1,25 +1,25 @@
 package com.capgemini.invoiceservice;
 
 public class InvoiceService {
-	
+
 	public static final double MINIMUM_COST_PER_KILOMETER = 10.0;
 	public static final int COST_PER_TIME = 1;
 	public static final double MINIMUM_FARE = 5.0;
-	
-	
+
 	public double calculateFare(double distance, int time) {
 		// TODO Auto-generated method stub
-		double totalFare = distance*MINIMUM_COST_PER_KILOMETER + COST_PER_TIME*time;
-	
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + COST_PER_TIME * time;
+
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
 
-
 	public InvoiceSummary calculateFare(Ride[] rides) {
 		// TODO Auto-generated method stub
-		
-		return null;
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return new InvoiceSummary(rides.length, totalFare);
 	}
-
 
 }
