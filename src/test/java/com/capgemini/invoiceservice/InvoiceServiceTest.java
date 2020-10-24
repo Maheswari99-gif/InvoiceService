276a2dfc3f6257843b2453890ea894c0213802e1
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-24 22:37:48 +0530

    MaheswariKoyyani : Added testcase to get minimum fare

--- src/test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
@@ -4,15 +4,23 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
-	private InvoiceService invoiceService;
 
 	@Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
+		InvoiceService invoiceService = new InvoiceService();
 		double distance = 2.0;
 		int time = 5;
-	    double fare = invoiceService.calculateFare(distance, time);
+		double fare = invoiceService.calculateFare(distance, time);
 		System.out.println(fare);
-		Assert.assertEquals(0, fare, 0.0);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+	@Test
+	public void givenLessDistanceOrTimeShouldReturnMinFare() {
+		InvoiceService invoiceService = new InvoiceService();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceService.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
 	}
 
 }
